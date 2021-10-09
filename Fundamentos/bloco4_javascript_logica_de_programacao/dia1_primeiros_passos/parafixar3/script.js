01c2ec025a662cffe0dfe57ef7c126e02bf4a682
Rafael <rafalyf11@gmail.com>
2021-10-09 15:07:41 -0300

    javascript base

---
+++ Fundamentos/bloco4_javascript_logica_de_programacao/dia1_primeiros_passos/parafixar3/script.js
@@ -0,0 +1,10 @@
+const notaFabio = 95;
+
+if (notaFabio >= 80) {
+  console.log("Aprovado")
+} else if (notaFabio < 80 && notaFabio >= 60) {
+  console.log("Lista de espera")
+}
+else {
+  console.log("Reprovado")
+}
