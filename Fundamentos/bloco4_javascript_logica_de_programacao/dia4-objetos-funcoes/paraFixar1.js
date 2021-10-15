97f97dd2c08845e04fe49f223265975cdc458493
Rafael <rafalyf11@gmail.com>
2021-10-15 19:11:35 -0300

    exercicios dias 4

---
+++ Fundamentos/bloco4_javascript_logica_de_programacao/dia4-objetos-funcoes/paraFixar1.js
@@ -0,0 +1,37 @@
+// let player = {
+//   name: 'Marta',
+//   lastName: 'Silva',
+//   age: 34,
+//   bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+//   medals: {
+//     golden: 2,
+//     silver: 3,
+//   }
+// }
+
+// console.log('A Jogadora ' + player.name + ' ' + player.lastName + ' ' + 'tem ' + player.age + ' ' + 'anos de idade')
+
+// console.log('A Jogadora ' + player.name + ' ' + player.lastName + ' ' + 'foi eleita a melhor do mundo por' + ' ' + player.bestInTheWorld.length + ' vezes')
+
+// console.log('a Jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata')
+
+// let names = {
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge' 
+// };
+
+// for ( key in names) {
+//   console.log('Ola ' + names[key])
+// }
+
+// let car = {
+//   model: 'A3 Sedan',
+//   manufacturer: 'Audi',
+//   year: 2020
+// };
+
+// for (let key in car) {
+//   console.log(key, car[key])
+// }
+
