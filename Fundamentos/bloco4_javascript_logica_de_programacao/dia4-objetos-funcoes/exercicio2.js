4fb11c26ad03953fbac71764a0d94c9fca88f693
Rafael <rafalyf11@gmail.com>
2021-10-18 00:09:50 -0300

    todos exercicios dia 4 (quase o 7) tirando os bonus

--- Fundamentos/bloco4_javascript_logica_de_programacao/dia4-objetos-funcoes/exercicio2.js
+++ Fundamentos/bloco4_javascript_logica_de_programacao/dia4-objetos-funcoes/exercicio2.js
@@ -53,20 +53,87 @@
 
 // Exercicio 4
 
-function somador(nomes) {
-  let maiorNome = '';
-  let nomeAtualI = '';
-  let nomeAtualS = '';
-  for (let i = 0; i < nomes.length; i += 1) {
-    for (let s = 1; s < nomes.length; s += 1) {
-    nomeAtualI = nomes[i].length
-    nomeAtualS = nomes[s].length
-    if (nomeAtualS > nomeAtualI) {
-      maiorNome = nomes[s]
+// function somador(nomes) {
+//   let maiorNome = '';
+//   let nomeAtualI = '';
+//   let nomeAtualS = '';
+//   for (let i = 0; i < nomes.length; i += 1) {
+//     for (let s = 1; s < nomes.length; s += 1) {
+//     nomeAtualI = nomes[i].length
+//     nomeAtualS = nomes[s].length
+//     if (nomeAtualS > nomeAtualI) {
+//       maiorNome = nomes[s]
+//     }
+//   }
+//   }
+//   return maiorNome
+// }
+
+// somador(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])
+
+// ============================
+
+// Exercicio 5
+
+// function repetente(numeros) {
+//   let numeroIgual = null;
+//   let quantasRepete = 0;
+//   for (let i = 0; i < numeros.length; i += 1) {
+//     let quantasVezes = 0;
+//     for (let s = i + 1; s < numeros.length; s += 1) {
+//       if (numeros[i] === numeros[s]) {
+//         quantasVezes += 1;
+
+//           if (quantasVezes > quantasRepete) {
+//             quantasRepete = quantasVezes
+//             numeroIgual = numeros[s]
+//           }
+//       }
+//     }
+//   }
+//   return numeroIgual
+// }
+
+// repetente([2, 3, 2, 5, 8, 2, 3, 3, 3, 3, 5, 5, 5, 5, 5])
+
+// ============================
+
+// Exercicio 6
+
+// function somatorio(n) {
+//   let soma = 0;
+//   for (i = 0; i <= n; i += 1) {
+//     soma += i
+//   }
+//   return soma
+// }
+
+// somatorio(5)
+
+// ============================
+
+// Exercicio 7
+
+function vericaFimPalavra (palavra, fimPalavra) {
+  let checagem = '';
+  let eOFim = null;
+  for (let i = 0; i < palavra.length; i += 1) {
+    for (let s = 0; s < fimPalavra.length; s += 1) {
+      if (fimPalavra[s] === palavra[i]) {
+        checagem += fimPalavra[s]
+        if (checagem === fimPalavra) {
+          eOFim = true;
+        } else {
+          eOFim = false;
+        }
+      }
     }
   }
-  }
-  return maiorNome
+  console.log(eOFim)
 }
 
-somador(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])
+vericaFimPalavra('trybe', 'be')
+mano = 'traibe'
+let conta = mano.length * 0.33
+
+// console.log(Math.round(conta))
