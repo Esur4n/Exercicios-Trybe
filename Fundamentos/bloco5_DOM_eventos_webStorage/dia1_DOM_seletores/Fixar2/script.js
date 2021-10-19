a6e91f02413c07f88eb3f6ea41abdd1b7122c4ed
Rafael <rafalyf11@gmail.com>
2021-10-19 19:08:06 -0300

    para fixar 2

---
+++ Fundamentos/bloco5_DOM_eventos_webStorage/dia1_DOM_seletores/Fixar2/script.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName('header')[0].style.backgroundColor = 'green';
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'lightpink';
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'hotpink';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'hotpink';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
+document.querySelector('footer').style.backgroundColor = 'darkgreen'
