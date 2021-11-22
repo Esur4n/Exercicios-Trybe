0cca46cb3f2822eb4c9989a1b113e5b23731057d
Rafael <rafalyf11@gmail.com>
2021-11-22 19:27:46 -0300

    parafixar 2

--- Fundamentos/bloco8/dia4/paraFixar1.js
+++ Fundamentos/bloco8/dia4/paraFixar1.js
@@ -1,7 +1,7 @@
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
 const aa = numbers.reduce((primeiro, segundo) => {
-  console.log(segundo);
-  (segundo % 2 === 0) ? primeiro + segundo : primeiro
+  return (segundo % 2 === 0) ? primeiro + segundo : primeiro
  }, 0);
 
+console.log(aa)
