7a6f8c02cd3bee97fd684b31a28dc98ed8f6bce1
Rafael <rafalyf11@gmail.com>
2021-11-23 19:52:53 -0300

    os 'paraFixar' de hoje

--- Fundamentos/bloco8/dia5/parafixar.js
+++ Fundamentos/bloco8/dia5/parafixar.js
@@ -7,4 +7,34 @@ const additionalItens = ['suco de laranja', 'gelo', 'nada'];
 const fruitSalad = (fruit, additional) => {
   return [...fruit, ...additional];
 }
-console.log(fruitSalad(specialFruit, additionalItens));
+// console.log(fruitSalad(specialFruit, additionalItens));
+
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+// console.log(product)
+
+const getNationality = ({ firstName, nationality = 'brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+// console.log(getNationality(otherPerson)); // Ivan is Russian
+// console.log(getNationality(person));
+
+const multiply = (number, value = 2) => {
+  return number * value
+};
+
+console.log(multiply(8));
