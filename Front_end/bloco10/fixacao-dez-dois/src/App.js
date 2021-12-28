4a8548567a14a5043620e92b3ea04bb99aaa52c0
Rafael <rafalyf11@gmail.com>
2021-12-27 22:40:13 -0300

    exercicio 2 de fixacao

--- Front_end/bloco10/fixacao-dez-dois/src/App.js
+++ Front_end/bloco10/fixacao-dez-dois/src/App.js
@@ -1,14 +1,36 @@
-// import logo from './logo.svg';
-import './App.css';
+// arquivo App.js, criado pelo create-react-app, modificado
 import React from 'react';
-import Image from './image.js'
+import './App.css';
+import Order from './Order';
 
 class App extends React.Component {
   render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
     return (
-      <>
-        <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='gatinho'/>
-      </>
+      <div className="App">
+        <h1> Orders recently created </h1>
+        <Order order={headphone}/>
+        <Order order={energyDrink}/>
+      </div>
     );
   }
 }
