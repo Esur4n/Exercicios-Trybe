a9dda731930376527f5b93522be859d8a2cc471e
Rafael <rafalyf11@gmail.com>
2022-02-07 19:12:06 -0300

    Merge branch 'master' of github.com:Esur4n/Exercicios-Trybe

---
+++ Front_end/bloco11/exercicios11.2/exercicios-onze-dois/src/Morada.js
@@ -0,0 +1,19 @@
+import React from "react";
+ 
+class Morada extends React.Component {
+ render() {
+   return <div>
+ 
+ <input type="radio" id="Casa" name="drone" value="Casa" checked/>
+ <label for="huey">Casa</label>
+ 
+ 
+ <input type="radio" id="Apartamento" name="drone" value="Apartamento"/>
+ <label for="dewey">Apartamento</label>
+ 
+ 
+</div>
+ 
+ }
+}
+export default Morada
