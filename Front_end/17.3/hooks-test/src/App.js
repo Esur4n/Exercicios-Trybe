9434d8671ec09cbb1fb16cd3e6788a3f0126fdc6
Rafael <rafalyf11@gmail.com>
2022-02-23 19:28:39 -0300

    tentativa paraFixar1

---
+++ Front_end/17.3/hooks-test/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [number, setNumber] = useState(1);
+  const [isMultiple, setMultiple] = useState(false);
+  const [timer, setTimer] = useState(1);
+
+  const TIMER_INTERVAL = 1000;
+  const NUMBER_TIMEOUT = 10000;
+  const MESSAGE_TIMEOUT = 4000;
+  const MIN_RANDOM = 1;
+  const MAX_RANDOM = 100;
+
+  const doMath = () => {
+    setNumber(Math.round(Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM)));
+  }
+
+  const testRandom = () => {
+    if (number % 5 === 0 || number % 3 === 0) {
+      setMultiple(true);
+      setTimeout(() => setMultiple(false), MESSAGE_TIMEOUT);
+    }
+    return setMultiple(false);
+  }
+
+  useEffect(() => {
+    testRandom();
+  }, [number])
+
+  return (
+    <div>
+      {isMultiple && 'certo'}
+    </div>
+  );
+}
+
+export default App;
